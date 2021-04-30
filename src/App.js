f49434fd6305ad8e66419cdbfba4725c968a888e
Radosław Urban <urban_radoslaw@o2.pl>
2021-04-30 10:31:42 +0200

    L8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom"
 import Posts from "./utils/components/posts";
 import PostsTable from "./utils/components/postsTable";
+import Home from "./utils/components/home"
+import Navbar from "./utils/components/navbar";
+import NotFound from "./utils/components/notFound";
 
 function App() {
   return (
-      <Posts/>
+    <div className="container-fluid">
+      <Navbar/>
+      <div className="container">
+          <div className="content">
+              <Switch>
+                  <Route path="/posts" render={props => <Posts sortBy="newest" {...props} />}/>
+                  <Route path="/" exact component={Home} />
+                  <Route component={NotFound} />
+              </Switch>
+          </div>
+      </div>
+    </div>
   );
-
 }
 
 export default App;
